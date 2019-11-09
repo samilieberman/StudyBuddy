9456ef309dab379dec1e33d3ef665e84c11ae9d4
Zokontech <elekid18@gmail.com>
2019-11-09 15:58:53 -0500

    This should fix the login showing with the nav bar

--- Posting.js
+++ Posting.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons'
+import { TouchableOpacity } from 'react-native-gesture-handler';
 Icon.loadFont();
 
 export default class Posting extends React.Component {
     render() {
         return (
             <View>
+                <TouchableOpacity style={styles.pic}/>
                 <Text style={styles.author}>{this.props.title}</Text>
                 <Text style={styles.message}>{this.props.desc}</Text>
             </View>
@@ -14,16 +16,27 @@ export default class Posting extends React.Component {
 }
 }
 const styles = StyleSheet.create({
+
+    pic:{
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        backgroundColor: 'red'
+    },
     container: {
         width: "100 %",
         //marginBottom: 5,
 
     },
     author: {
+        flex: 2, 
+        flexDirection: 'row',
         color: "gray",
         fontSize: 25
     },
     message: {
+        flex: 2, 
+        flexDirection: 'row',
         color: "black",
         fontSize: 20
     }
