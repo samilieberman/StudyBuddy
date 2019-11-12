19d6f02f93f82f0e7970440a56889dcb34f536c4
samilieberman <samilieberman1@gmail.com>
2019-11-12 15:25:40 -0500

    hide the API key

--- firebase.js
+++ firebase.js
@@ -1,8 +1,9 @@
 import * as firebase from 'firebase'  // Should not be used elsewhere in the project
 import firebaseData from './app.json';
+import config from './config.js';
 
 firebase.initializeApp({
-    apiKey: firebaseData.expo.extra.firebase.apiKey,
+    apiKey: config.apiKey,
     authDomain: firebaseData.expo.extra.firebase.authDomain,
     databaseURL: firebaseData.expo.extra.firebase.databaseURL,
     projectId: firebaseData.expo.extra.firebase.projectId,
